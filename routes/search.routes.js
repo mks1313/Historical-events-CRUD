c74d205feeaccb663ece8133b8c3e6e59a26c128
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-02-07 20:38:31 +0100

    search-bar

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -2,25 +2,23 @@ const express = require('express');
 const router = express.Router();
 const { HistoricalEvent } = require("../models/HistoricalEvent.model");
 
-router.get("/event-archive", (req, res) => {
+router.get("/event-results", (req, res) => {
     const query = req.query.query; 
     console.log("Query:", query);
     if (query) {
-        // Si hay una consulta de búsqueda, encontrar eventos que coincidan
+   
         HistoricalEvent.find({ $text: { $search: query } }) 
             .then(events => {
-                // Renderizar la plantilla con los eventos que coinciden con la búsqueda
-                res.render('events/event-archive', { events });
+                res.render('events/event-rusults', { events });
             })
             .catch(error => {
                 console.error(error);
                 res.status(500).send('Internal Server Error');
             });
     } else {
-        // Si no hay consulta de búsqueda, renderizar la plantilla con todos los eventos
         HistoricalEvent.find() 
             .then(events => {
-                res.render('events/event-archive', { events });
+                res.render('events/event-rusults', { events });
             })
             .catch(error => {
                 console.error(error);
