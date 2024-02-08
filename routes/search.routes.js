9fc921c7b95f10307676243759b12605591dd6aa
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-02-08 14:20:01 +0100

    acabada searchbar

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -1,20 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { HistoricalEvent } = require("../models/HistoricalEvent.model");
 
 router.get("/search-results", (req, res) => {
-    const query = req.query.query; // Captura la consulta de búsqueda del parámetro de la URL
-
-    // Realiza una búsqueda en la base de datos para eventos cuyos títulos coincidan con la consulta
-    HistoricalEvent.find({ title: { $regex: query, $options: 'i' } })
-        .then(events => {
-            res.render('search-results', { events, query }); // Renderiza la vista con los resultados de la búsqueda
-        })
-        .catch(error => {
-            console.error(error);
-            res.render('error', { message: 'Error searching for events' });
-        });
+  const query = req.query.query;
+  HistoricalEvent.find({ title: { $regex: query, $options: "i" } })
+    .then((events) => {
+      res.render("search-results", { events, query });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.render("error", { message: "Error searching for events" });
+    });
 });
 
 module.exports = router;
-
