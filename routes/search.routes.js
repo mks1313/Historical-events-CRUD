0e3433c3f05a42860a39ec7c139df76a150571a0
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-02-08 14:17:17 +0100

    searchbar-acabado

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -2,31 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { HistoricalEvent } = require("../models/HistoricalEvent.model");
 
-router.get("/event-results", (req, res) => {
-    const query = req.query.query; 
-    console.log("Query:", query);
-    if (query) {
-   
-        HistoricalEvent.find({ $text: { $search: query } }) 
-            .then(events => {
-                res.render('events/event-rusults', { events });
-            })
-            .catch(error => {
-                console.error(error);
-                res.status(500).send('Internal Server Error');
-            });
-    } else {
-        HistoricalEvent.find() 
-            .then(events => {
-                res.render('events/event-rusults', { events });
-            })
-            .catch(error => {
-                console.error(error);
-                res.status(500).send('Internal Server Error');
-            });
-    }
+router.get("/search-results", (req, res) => {
+    const query = req.query.query; // Captura la consulta de búsqueda del parámetro de la URL
+
+    // Realiza una búsqueda en la base de datos para eventos cuyos títulos coincidan con la consulta
+    HistoricalEvent.find({ title: { $regex: query, $options: 'i' } })
+        .then(events => {
+            res.render('search-results', { events, query }); // Renderiza la vista con los resultados de la búsqueda
+        })
+        .catch(error => {
+            console.error(error);
+            res.render('error', { message: 'Error searching for events' });
+        });
 });
 
-
-
 module.exports = router;
+
