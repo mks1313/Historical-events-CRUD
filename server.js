1b28b8cb98905269feffe0cd37afa3e10328f224
Marshall-Bits <kz_@hotmail.es>
2024-02-09 17:00:08 +0100

    uninstall browser-sync

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require("./index.js");
+const app = require("./app.js");
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
